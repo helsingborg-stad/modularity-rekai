80e6453cb88b50dfa8115b7223638cf4a51f4a03
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-02-17 11:48:17 +0100

    ci: update npm packages, fix webpack config for v5, update build.php to use npm ci, add build timer

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const webpack = require('webpack');
-const ManifestPlugin = require('webpack-manifest-plugin');
+
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const RemoveEmptyScripts = require('webpack-remove-empty-scripts');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const {getIfUtils, removeEmpty} = require('webpack-config-utils');
-const {ifProduction, ifNotProduction} = getIfUtils(process.env.NODE_ENV);
+const { ifProduction } = getIfUtils(process.env.NODE_ENV);
 
 module.exports = {
     mode: ifProduction('production', 'development'),
@@ -17,15 +17,16 @@ module.exports = {
      * Add your entry files here
      */
     entry: {
-        'css/modularity-recommend':               './source/sass/modularity-recommend.scss',
+        'css/modularity-recommend': './source/sass/modularity-recommend.scss',
     },
     
     /**
      * Output settings
      */
     output: {
-        filename: ifProduction('[name].min.js', '[name].min.js'),
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
     },
     /**
      * Define external dependencies here
@@ -45,21 +46,19 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             importLoaders: 3, // 0 => no loaders (default); 1 => postcss-loader; 2 => sass-loader
-                            sourceMap: true,
                         },
                     },
                     {
                         loader: 'postcss-loader',
                         options: {
-                            // plugins: [autoprefixer, require('postcss-object-fit-images')],
-                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [autoprefixer],
+                            }
                         },
                     },
                     {
                         loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        }
+                        options: {}
                     },
                     'import-glob-loader'
                 ],
@@ -70,7 +69,7 @@ module.exports = {
         /**
          * Fix CSS entry chunks generating js file
          */
-        new FixStyleOnlyEntriesPlugin(),
+        new RemoveEmptyScripts(),
 
         /**
          * Clean dist folder
@@ -80,13 +79,13 @@ module.exports = {
          * Output CSS files
          */
         new MiniCssExtractPlugin({
-            filename: ifProduction('[name].min.css', '[name].min.css')
+            filename: '[name].[contenthash].css'
         }),
 
         /**
          * Output manifest.json for cache busting
          */
-        new ManifestPlugin({
+        new WebpackManifestPlugin({
             // Filter manifest items
             filter: function (file) {
                 // Don't include source maps
@@ -117,11 +116,6 @@ module.exports = {
         }),
 
         /**
-         * Required to enable sourcemap from node_modules assets
-         */
-        new webpack.SourceMapDevToolPlugin(),
-
-        /**
          * Enable build OS notifications (when using watch command)
          */
         new WebpackNotifierPlugin({alwaysNotify: true, skipFirstNotification: true}),
@@ -129,13 +123,18 @@ module.exports = {
         /**
          * Minimize CSS assets
          */
-        ifProduction(new OptimizeCssAssetsPlugin({
-            cssProcessorPluginOptions: {
-                preset: ['default', {discardComments: {removeAll: true}}],
+        ifProduction(new CssMinimizerWebpackPlugin({
+            minimizerOptions: {
+                preset: [
+                    "default",
+                    {
+                        discardComments: { removeAll: true },
+                    },
+                ],
             },
-        })),
+        }))
 
     ]).filter(Boolean),
-    devtool: ifProduction('none', 'eval-source-map'),
+    devtool: 'source-map',
     stats: {children: false}
 };
